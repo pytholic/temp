7308cd41da42b557e2bc2ee1eddaa863c664235e
Raja Haseeb <rajahaseeb147@gmail.com>
2022-11-16 13:42:32 +0900

    Add files via upload

--- vis_filtering.py
+++ vis_filtering.py
@@ -3,7 +3,9 @@ import time
 import numpy as np
 
 import open3d as o3d
+
 from functools import partial
+from collections import defaultdict
 
 
 # Define paths and variables
@@ -11,14 +13,14 @@ from functools import partial
 PATH = './results'
 ZOOM = 1
 COLORS = { 
-    "person": [0,255,0],
-    "chair": [0,191,255],
-    "mouse": [80.0,127.0,255.0],
-    "keyboard": [99,49,222],
-    "phone": [191,226,191],
-    "cup": [208,224,64],
-    "laptop": [237,149,100],
-    "tv": [255,204,204]
+    'person': [0,255,0],
+    'chair': [0,191,255],
+    'mouse': [80.0,127.0,255.0],
+    'keyboard': [99,49,222],
+    'phone': [191,226,191],
+    'cup': [208,224,64],
+    'laptop': [237,149,100],
+    'tv': [255,204,204]
 }
 
 
@@ -56,65 +58,35 @@ def get_indices(pcd, category):
 	indices = []
 	colors_original = {}
 	for i, color in enumerate(pcd.colors):
-		color = color*255
+		color = color*255  # denormalize
 		res = is_close(list(color), COLORS[category][::-1])
 		if res == True:
 			indices.append(i)
-			colors_original[i]=color/255
+			colors_original[i]=color/255  # normalize
 	return indices, colors_original
 
-
-cnt_mouse = 0
-cnt_keyboard = 0
 
 def filter(vis, cls):
 	"""
 	Function to filter the desired objects.
 	"""
+	global cnt_dict
 
-	if cls == 1:
-		
-		global cnt_mouse
-		cnt_mouse+=1
+	cnt_dict[f'{cls}']+=1
 
-		if cnt_mouse%2 != 0:	
-			for i in mouse_indices:
-				pcd.colors[i] = [1,1,1]
+	# Set mask color if count is odd
+	if cnt_dict[f'{cls}']%2 != 0:
+		for i in idx_dict[f'{cls}'][0]:
+			pcd.colors[i] = [1,1,1]
+		print(f"{cls} filtering done...")
 
-			print("Mouse filtering done...")
-
-		else:
-			for i in mouse_indices:
-				pcd.colors[i] = orig_mouse[i]
-
-			print("Mouse filtering removed...")
-
-	if cls == 2:
-
-		global cnt_keyboard
-		cnt_keyboard+=1
-
-		if cnt_keyboard%2 != 0:	
-			for i in keyboard_indices:
-				pcd.colors[i] = [1,1,1]
-
-			print("Keyboard filtering done...")
-
-		else:
-			for i in keyboard_indices:
-				pcd.colors[i] = orig_keyboard[i]
-
-			print("Keyboard filtering removed...")
+	# Set default color if count is even
+	else:
+		for i in idx_dict[f'{cls}'][0]:
+			pcd.colors[i] = idx_dict[f'{cls}'][1][i]
+		print(f"{cls} filtering removed...")
 
 	vis.update_geometry(pcd)
-
-
-def create_point_cloud(points, colors):
-    """ open3d point cloud from numpy array """
-    point_cloud = o3d.geometry.PointCloud()
-    point_cloud.points = o3d.utility.Vector3dVector(points)
-    point_cloud.colors = o3d.utility.Vector3dVector(colors)
-    return point_cloud
 
 
 # def show_original(vis):
@@ -123,16 +95,29 @@ def create_point_cloud(points, colors):
 # 	print("Showing original...")
 # 	vis.update_geometry(pcd)
 
+
 # Read point clouds
 # #pcd = o3d.io.read_point_cloud(os.path.join(PATH, 'points.ply'))
 pcd = o3d.io.read_point_cloud(os.path.join(PATH, 'output.ply'))
 poses = o3d.io.read_line_set(os.path.join(PATH, 'camera.ply'))
-pcd_copy = create_point_cloud(pcd.points, pcd.colors)
+
+
+# Create counter dict
+cnt_dict = defaultdict()
+cnt_dict['mouse'] = 0
+cnt_dict['keyboard'] = 0
 
 # Check condition for objects and save indices
-mouse_indices, orig_mouse = get_indices(pcd, "mouse")
-keyboard_indices, orig_keyboard = get_indices(pcd, "keyboard")
+mouse_indices, orig_mouse = get_indices(pcd, 'mouse')
+keyboard_indices, orig_keyboard = get_indices(pcd, 'keyboard')
 
+# Create indices dict
+idx_dict = dict()
+idx_dict['mouse'] = [mouse_indices, orig_mouse]
+idx_dict['keyboard'] = [keyboard_indices, orig_keyboard]
+
+
+# Visualization
 vis = o3d.visualization.VisualizerWithKeyCallback()
 vis.create_window()
 
@@ -176,19 +161,19 @@ view_ctl = vis.get_view_control()
 view_ctl.set_up((0, -1, 0))
 view_ctl.set_zoom(.5)
 
-vis.register_key_callback(ord("W"), zoom_in)
-vis.register_key_callback(ord("S"), zoom_out)
+vis.register_key_callback(ord('W'), zoom_in)
+vis.register_key_callback(ord('S'), zoom_out)
 
-vis.register_key_callback(ord("A"), rotate_view_left)
-vis.register_key_callback(ord("D"), rotate_view_right)
+vis.register_key_callback(ord('A'), rotate_view_left)
+vis.register_key_callback(ord('D'), rotate_view_right)
 
-vis.register_key_callback(ord("Q"), rotate_view_up)
-vis.register_key_callback(ord("E"), rotate_view_down)
+vis.register_key_callback(ord('Q'), rotate_view_up)
+vis.register_key_callback(ord('E'), rotate_view_down)
 
-vis.register_key_callback(ord("6"), partial(filter, cls=1))
-vis.register_key_callback(ord("7"), partial(filter, cls=2))
+vis.register_key_callback(ord('6'), partial(filter, cls='mouse'))
+vis.register_key_callback(ord('7'), partial(filter, cls='keyboard'))
 
-#vis.register_key_callback(ord("8"), show_original)
+#vis.register_key_callback(ord('8'), show_original)
 
 vis.add_geometry(pcd)
 vis.add_geometry(poses)
